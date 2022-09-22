ba1f33cf8a39cfafbef3cc77e6e577d37b14ad28
Shweta0408 <Shmis2004@gmail.com>
2022-09-22 20:52:24 +0530

    challenge half

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -12,7 +12,7 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  /*
+  
   const arr = [2, 3, 4];
   const a = arr[0];
   const b = arr[0];
@@ -30,12 +30,13 @@ const restaurant = {
   const [starter, mainCourse] = restaurant.order(2, 0);
   console.log(starter, mainCourse);
   */
-  // const temp = main;
-  // main = secondary;
-  // secondary = temp;
-  // console.log(main, secondary);
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
 
-  //;
+//;
+/*
   openingHours: {
     thu: {
       open: 12,
@@ -272,3 +273,42 @@ rest1.owner &&= '<ANONYMOUS>';
 rest2.owner &&= '<Anonymous>';
 
 console.log(rest1, rest2);
+
+
+*/
+
+//coding challenge
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrusia Dortmund',
+  players: [
+    [
+      'Never',
+      'Pavard',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretza',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+
+    [
+      'Burki',
+      'schulz',
+      'Hummels',
+      'Akanji',
+      'hakimi',
+      'Weigl',
+      'witsel',
+      'Hazard',
+      'Brandt',
+    ],
+  ],
+};
+
+const playerFinal = ['thiago', 'Coutinho', 'Perisic', ...game.team1.players];
+console.log(playerFinal);
